dc2dbd421a8c5a16bd91fe1859edc79e55244723
atindraraut <atindraraut80@gmail.com>
2024-11-13 22:17:54 +0530

    inital commit after configuring the terraform remote state and remote lock in s3 and dynamodb

---
+++ provider.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    dynamodb_table = "state-lock"
+    bucket = "s3statebackendatindra"
+    key = "global/mystatefile/terrafrom.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# create s3
+resource "aws_s3_bucket" "mybucket"{
+    bucket  = "s3statebackendatindra"
+    versioning {
+        enabled = true
+    }
+
+}
+
+
+# create dynamodb
+resource "aws_dynamodb_table" "statelock" {
+  name = "state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
